2e98267ee9d7316827d5002bc1b20a765181e4e2
Isaac Dias <m.dias.isaac@gmail.com>
2023-07-26 21:52:33 -0300

    atualizacoes isaac

---
+++ av4/bloco_anonimo.sql
@@ -0,0 +1,18 @@
+DECLARE
+    v_atracao Atracao%ROWTYPE;
+	
+BEGIN
+    v_atracao.nome := 'Show de Violino';
+	v_atracao.colaborante := null;		
+	v_atracao.cache := '5000';
+         -- Inserção na tabela Atracao
+    INSERT INTO Atracao VALUES v_atracao;
+
+	DBMS_OUTPUT.PUT_LINE('Inserção realizada com sucesso!');
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Ocorreu um erro: ' || SQLERRM);
+
+END;
+/
---
+++ av4/loop_case_when_elsif_cursor.sql
@@ -0,0 +1,38 @@
+DECLARE 
+    v_atracao show.atracao%TYPE;
+    v_palco show.palco%TYPE;
+    v_horario show.horario%TYPE;
+    v_idtec show.id_tecn%TYPE;
+
+    CURSOR c_show IS 
+        SELECT atracao, palco, horario, id_tecn FROM show
+        ORDER BY id_tecn;
+    
+BEGIN
+    OPEN c_show;
+    LOOP
+        FETCH c_show INTO v_atracao, v_palco, v_horario, v_idtec;
+
+        CASE v_palco
+            WHEN '1' THEN
+                IF v_idtec = '22307947437' THEN
+                    DBMS_OUTPUT.PUT_LINE('Nome da atração alocada ao palco 1: ' || v_atracao);
+                    DBMS_OUTPUT.PUT_LINE('Horario: ' || v_horario);
+                END IF;
+            WHEN '2' THEN
+                IF v_idtec = '22307947437' THEN
+                    DBMS_OUTPUT.PUT_LINE('Nome da atração alocada ao palco 2: ' || v_atracao);
+                    DBMS_OUTPUT.PUT_LINE('Horario: ' || v_horario);
+                END IF;
+            WHEN '3' THEN
+                DBMS_OUTPUT.PUT_LINE('Palco 3: ' || v_atracao || ' - Horario: ' || v_horario);
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('Buscando...');
+        END CASE;
+        
+        EXIT WHEN c_show%NOTFOUND;
+    END LOOP;
+    
+    CLOSE c_show;
+END;
+/
