dc99d96b1250d44ca7603f7dd5025b66681ee8cb
Isaac Dias <m.dias.isaac@gmail.com>
2023-07-31 15:20:02 -0300

    comentario

--- av4/having_count.sql
+++ av4/having_count.sql
@@ -1,3 +1,5 @@
+-- A consulta a seguir tem como objetivo contar quantos funcionários têm mais de 4 vendas (ou seja, compraram ingressos) por turno.
+--  Caso um funcionário tenha mais de uma compra registrada na tabela "Compra" ou mais de um ingresso registrado na tabela "Ingresso", e ambos estão relacionados ao mesmo funcionário através das condições de junção especificadas nos LEFT OUTER JOINs. 
 SELECT F.turno, COUNT(F.id_func)
 FROM Funcionario F 
 LEFT OUTER JOIN Compra C ON F.id_func = C.vendedor -- LEFT OUTER JOIN
