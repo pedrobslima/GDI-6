b7c42aba8dc11c5d66d92da0f5a4f32bfbe71d56
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-08-30 19:39:45 -0300

    'disponibiliza' consulta 'nome_preco'

--- av6/consulta_disponibiliza.sql
+++ av6/consulta_disponibiliza.sql
@@ -1,9 +1,13 @@
---	CONSULTA EQUIPAMENTOS QUE ESTÃO DISPONIVEIS NO PALCO 'X'
+--	CONSULTA EQUIPAMENTOS, COM NOME E PRECO, QUE ESTÃO DISPONIVEIS NO PALCO 'X'
 
 SELECT
     D.palco.numero AS "PALCO",
-    DEREF(D.equipamento.nome).nome AS "EQUIPAMENTO"
+    DEREF(D.equipamento.nome).nome AS "EQUIPAMENTO",
+    P.preco
 FROM
-    tb_disponibiliza D
+    tb_disponibiliza D,
+    tb_nome_preco P
 WHERE
-    D.palco.numero = 2;
+    D.palco.numero = 2
+    AND
+    DEREF(D.equipamento.nome).nome = P.nome;
