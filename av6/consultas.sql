68e929cdeaee6b88be91246c2cf17d0666e4a2c7
Isaac Dias <87576126+diasisaac@users.noreply.github.com>
2023-08-30 22:05:05 -0300

    Update consultas.sql

--- av6/consultas.sql
+++ av6/consultas.sql
@@ -3,9 +3,3 @@
                     --   vvvvvvvvvvvv MAP MEMBER FUNCTION venda_total:
 SELECT DP.dia_evento, DP.venda_total() AS total_de_vendas FROM tb_dia_preco DP
 
-
--- [TESTAR] - Mostre quantos clientes têm por bairro,  e quantos deles tem o cep igual ao de João Francisco
-SELECT COUNT(*), COUNT(DEREF(tb_visitante.endereco).cep), DEREF(tb_visitante.endereco).bairro, as bairro
-FROM tb_visitante GROUP BY (DEREF(tb_visitante.endereco).bairro)
-HAVING DEREF(tb_visitante.endereco).cep = (SELECT DEREF(tb_visitante.endereco).cep FROM tb_visitante 
-    where REF(tb_visitante).nome = 'João Francisco'));
