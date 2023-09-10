735883d6521118a4ac08243d173273d78b716ad0
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-09-10 17:11:45 -0300

    novo 'idiomas'

---
+++ mongodb/_idiomas.js
@@ -0,0 +1,26 @@
+db.idiomas.insertMany([
+   {
+      lingua: "francês",
+      atividades:[{
+         tema: "Saudações",
+         descricao: "Aprender a falar francês",
+         nivel: "básico",
+         qntd_questoes: 10
+      },{
+         tema: "Cores",
+         descricao: "Aprender as principais cores",
+         nivel: "básico",
+         qntd_questoes: 5
+      }]
+   },{
+      lingua: "inglês",
+      atividades:[{
+         tema: "Saudações",
+         descricao: "Aprender a falar inglês",
+         nivel: "básico",
+         qntd_questoes: 10
+      }]
+   }
+
+
+])
