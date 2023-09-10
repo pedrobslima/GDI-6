944f164e228535887ace12f297c79beff4baa5bc
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-09-10 17:54:36 -0300

    povoamento incrementado

--- mongodb/_idiomas.js
+++ mongodb/_idiomas.js
@@ -1,26 +1,135 @@
 db.idiomas.insertMany([
    {
       lingua: "francês",
-      atividades:[{
-         tema: "Saudações",
-         descricao: "Aprender a falar francês",
-         nivel: "básico",
-         qntd_questoes: 10
-      },{
-         tema: "Cores",
-         descricao: "Aprender as principais cores",
-         nivel: "básico",
-         qntd_questoes: 5
-      }]
-   },{
+      atividades: [
+         {
+            tema: "Saudações",
+            descricao: "Aprender a falar francês",
+            nivel: "básico",
+            qntd_questoes: 10
+         },
+         {
+            tema: "Cores",
+            descricao: "Aprender as principais cores",
+            nivel: "básico",
+            qntd_questoes: 5
+         },
+         {
+            tema: "Alimentos",
+            descricao: "Aprender nomes de alimentos em francês",
+            nivel: "intermediário",
+            qntd_questoes: 8
+         },
+         {
+            tema: "Viagem",
+            descricao: "Vocabulário útil para viajar na França",
+            nivel: "intermediário",
+            qntd_questoes: 7
+         }
+      ]
+   },
+   {
       lingua: "inglês",
-      atividades:[{
-         tema: "Saudações",
-         descricao: "Aprender a falar inglês",
-         nivel: "básico",
-         qntd_questoes: 10
-      }]
+      atividades: [
+         {
+            tema: "Saudações",
+            descricao: "Aprender a falar inglês",
+            nivel: "básico",
+            qntd_questoes: 10
+         },
+         {
+            tema: "Números",
+            descricao: "Aprender a contar em inglês",
+            nivel: "básico",
+            qntd_questoes: 5
+         },
+         {
+            tema: "Família",
+            descricao: "Vocabulário relacionado à família",
+            nivel: "intermediário",
+            qntd_questoes: 8
+         }
+      ]
+   },
+   {
+      lingua: "espanhol",
+      atividades: [
+         {
+            tema: "Saudações",
+            descricao: "Aprender a falar espanhol",
+            nivel: "básico",
+            qntd_questoes: 10
+         },
+         {
+            tema: "Comida",
+            descricao: "Aprender nomes de comidas em espanhol",
+            nivel: "básico",
+            qntd_questoes: 6
+         },
+         {
+            tema: "Cinema",
+            descricao: "Vocabulário relacionado ao cinema em espanhol",
+            nivel: "intermediário",
+            qntd_questoes: 9
+         }
+      ]
+   },
+   {
+      lingua: "alemão",
+      atividades: [
+         {
+            tema: "Saudações",
+            descricao: "Aprender a falar alemão",
+            nivel: "básico",
+            qntd_questoes: 10
+         },
+         {
+            tema: "Transporte",
+            descricao: "Vocabulário relacionado a meios de transporte em alemão",
+            nivel: "básico",
+            qntd_questoes: 7
+         },
+         {
+            tema: "Música",
+            descricao: "Conhecer músicas populares em alemão",
+            nivel: "intermediário",
+            qntd_questoes: 8
+         },
+         {
+            tema: "Cultura",
+            descricao: "Fique por dentro da cultura alemã",
+            nivel: "intermediário",
+            qntd_questoes: 8
+         }
+      ]
+   },
+   {
+      lingua: "italiano",
+      atividades: [
+         {
+            tema: "Saudações",
+            descricao: "Aprender a falar italiano",
+            nivel: "básico",
+            qntd_questoes: 10
+         },
+         {
+            tema: "Esportes",
+            descricao: "Vocabulário relacionado a esportes em italiano",
+            nivel: "básico",
+            qntd_questoes: 6
+         },
+         {
+            tema: "Arte",
+            descricao: "Conhecer obras de arte italianas famosas",
+            nivel: "intermediário",
+            qntd_questoes: 9
+         },
+         {
+            tema: "Culinaria",
+            descricao: "Aprender nomes de comidas em italiano",
+            nivel: "intermediário",
+            qntd_questoes: 9
+         }
+      ]
    }
-
-
-])
+]);
