31c1dd3ec9b83bb7191863d161553e22d1395449
Pedro Barros de Souza Lima <101725415+pbsl-cin@users.noreply.github.com>
2023-09-10 18:41:43 -0300

    povoamento correto

---
+++ mongodb/alunos.js
@@ -0,0 +1,347 @@
+db.alunos.insertMany([
+    {
+        nome: "Felipe Tavares",
+        cpf: "555666777-88",
+        aniversario: new Date("1996-12-17"),
+        cursos: [
+            {
+                idioma: "chines",
+                progresso: 0.5,
+                ult_atividade: 2
+            },
+            {
+                idioma: "ingles",
+                progresso: 0.7,
+                ult_atividade: 5
+            }
+        ]
+    },
+    {
+        nome: "Mariana Silva",
+        cpf: "123456789-00",
+        aniversario: new Date("1998-05-20"),
+        cursos: [
+            {
+                idioma: "espanhol",
+                progresso: 0.4,
+                ult_atividade: 3
+            },
+            {
+                idioma: "frances",
+                progresso: 0.6,
+                ult_atividade: 4
+            }
+        ]
+    },
+    {
+        nome: "Lucas Oliveira",
+        cpf: "987654321-11",
+        aniversario: new Date("1995-08-12"),
+        cursos: [
+            {
+                idioma: "alemão",
+                progresso: 0.3,
+                ult_atividade: 2
+            },
+            {
+                idioma: "italiano",
+                progresso: 0.8,
+                ult_atividade: 6
+            }
+        ]
+    },
+    {
+        nome: "Amanda Santos",
+        cpf: "111222333-44",
+        aniversario: new Date("2000-03-25"),
+        cursos: [
+            {
+                idioma: "espanhol",
+                progresso: 0.6,
+                ult_atividade: 4
+            },
+            {
+                idioma: "frances",
+                progresso: 0.5,
+                ult_atividade: 3
+            }
+        ]
+    },
+    {
+        nome: "Rafael Pereira",
+        cpf: "555555555-55",
+        aniversario: new Date("1997-11-10"),
+        cursos: [
+            {
+                idioma: "chines",
+                progresso: 0.7,
+                ult_atividade: 6
+            },
+            {
+                idioma: "espanhol",
+                progresso: 0.4,
+                ult_atividade: 2
+            }
+        ]
+    },
+    {
+        nome: "Camila Souza",
+        cpf: "777888999-00",
+        aniversario: new Date("1999-07-15"),
+        cursos: [
+            {
+                idioma: "ingles",
+                progresso: 0.9,
+                ult_atividade: 7
+            },
+            {
+                idioma: "frances",
+                progresso: 0.6,
+                ult_atividade: 4
+            }
+        ]
+    },
+    {
+        nome: "Pedro Santos",
+        cpf: "222333444-55",
+        aniversario: new Date("1994-04-02"),
+        cursos: [
+            {
+                idioma: "italiano",
+                progresso: 0.8,
+                ult_atividade: 5
+            },
+            {
+                idioma: "espanhol",
+                progresso: 0.7,
+                ult_atividade: 6
+            }
+        ]
+    },
+    {
+        nome: "Isabella Lima",
+        cpf: "666777888-99",
+        aniversario: new Date("1993-09-08"),
+        cursos: [
+            {
+                idioma: "ingles",
+                progresso: 0.5,
+                ult_atividade: 3
+            },
+            {
+                idioma: "frances",
+                progresso: 0.4,
+                ult_atividade: 2
+            }
+        ]
+    },
+    {
+        nome: "João Silva",
+        cpf: "123123123-12",
+        aniversario: new Date("1996-02-14"),
+        cursos: [
+            {
+                idioma: "chines",
+                progresso: 0.3,
+                ult_atividade: 2
+            },
+            {
+                idioma: "italiano",
+                progresso: 0.6,
+                ult_atividade: 4
+            }
+        ]
+    },
+    {
+        nome: "Carla Oliveira",
+        cpf: "999888777-66",
+        aniversario: new Date("2001-06-30"),
+        cursos: [
+            {
+                idioma: "espanhol",
+                progresso: 0.7,
+                ult_atividade: 5
+            },
+            {
+                idioma: "frances",
+                progresso: 0.5,
+                ult_atividade: 3
+            }
+        ]
+    },
+    {
+        nome: "Marcos Ferreira",
+        cpf: "444555666-77",
+        aniversario: new Date("1998-12-05"),
+        cursos: [
+            {
+                idioma: "ingles",
+                progresso: 0.9,
+                ult_atividade: 7
+            },
+            {
+                idioma: "italiano",
+                progresso: 0.8,
+                ult_atividade: 6
+            }
+        ]
+    },
+    {
+        nome: "Larissa Pereira",
+        cpf: "111222338-44",
+        aniversario: new Date("1997-03-22"),
+        cursos: [
+            {
+                idioma: "chines",
+                progresso: 0.6,
+                ult_atividade: 4
+            },
+            {
+                idioma: "espanhol",
+                progresso: 0.5,
+                ult_atividade: 3
+            }
+        ]
+    },
+    {
+        nome: "Gustavo Martins",
+        cpf: "888777666-55",
+        aniversario: new Date("1999-08-18"),
+        cursos: [
+            {
+                idioma: "frances",
+                progresso: 0.7,
+                ult_atividade: 5
+            },
+            {
+                idioma: "italiano",
+                progresso: 0.6,
+                ult_atividade: 4
+            }
+        ]
+    },
+    {
+        nome: "Maria Lima",
+        cpf: "333444555-66",
+        aniversario: new Date("1995-10-09"),
+        cursos: [
+            {
+                idioma: "ingles",
+                progresso: 0.8,
+                ult_atividade: 6
+            },
+            {
+                idioma: "chines",
+                progresso: 0.4,
+                ult_atividade: 2
+            }
+        ]
+    },
+    {
+        nome: "Ricardo Santos",
+        cpf: "777666555-44",
+        aniversario: new Date("1994-04-27"),
+        cursos: [
+            {
+                idioma: "espanhol",
+                progresso: 0.5,
+                ult_atividade: 3
+            },
+            {
+                idioma: "frances",
+                progresso: 0.4,
+                ult_atividade: 2
+            }
+        ]
+    },
+    {
+        nome: "Ana Oliveira",
+        cpf: "222333494-55",
+        aniversario: new Date("2000-01-15"),
+        cursos: [
+            {
+                idioma: "italiano",
+                progresso: 0.7,
+                ult_atividade: 5
+            },
+            {
+                idioma: "espanhol",
+                progresso: 0.6,
+                ult_atividade: 4
+            }
+        ]
+    },
+    {
+        nome: 'Ana Silva',
+        cpf: '123456789-10',
+        aniversario: ISODate("1995-08-23T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'espanhol', progresso: 0.3, ult_atividade: 1 },
+            { idioma: 'italiano', progresso: 0.2, ult_atividade: 2 }
+        ]
+    },
+    {
+        nome: 'João Santos',
+        cpf: '987654321-61',
+        aniversario: ISODate("1998-05-10T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'alemão', progresso: 0.15, ult_atividade: 1 },
+            { idioma: 'inglês', progresso: 0.6, ult_atividade: 3 }
+        ]
+    },
+    {
+        nome: 'Maria Oliveira',
+        cpf: '734888999-22',
+        aniversario: ISODate("1997-03-05T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'francês', progresso: 0.5, ult_atividade: 3 },
+            { idioma: 'espanhol', progresso: 0.4, ult_atividade: 2 }
+        ]
+    },
+    {
+        nome: 'Pedro Lima',
+        cpf: '234567814-44',
+        aniversario: ISODate("1999-07-21T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'italiano', progresso: 0.3, ult_atividade: 2 },
+            { idioma: 'francês', progresso: 0.2, ult_atividade: 1 }
+        ]
+    },
+    {
+        nome: 'Laura Mendes',
+        cpf: '444123666-77',
+        aniversario: ISODate("1994-11-12T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'inglês', progresso: 0.4, ult_atividade: 3 },
+            { idioma: 'espanhol', progresso: 0.5, ult_atividade: 4 }
+        ]
+    },
+    {
+        nome: 'Carlos Ferreira',
+        cpf: '888999050-11',
+        aniversario: ISODate("2000-02-28T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'alemão', progresso: 0.25, ult_atividade: 1 },
+            { idioma: 'francês', progresso: 0.6, ult_atividade: 2 }
+        ]
+    },
+    {
+        nome: 'Ana Santos',
+        cpf: '123123999-12',
+        aniversario: ISODate("1996-09-15T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'espanhol', progresso: 0.4, ult_atividade: 1 },
+            { idioma: 'italiano', progresso: 0.3, ult_atividade: 3 }
+        ]
+    },
+    {
+        nome: 'Mariana Costa',
+        cpf: '999888777-69',
+        aniversario: ISODate("1998-04-03T00:00:00.000Z"),
+        cursos: [
+            { idioma: 'alemão', progresso: 0.2, ult_atividade: 2 },
+            { idioma: 'inglês', progresso: 0.5, ult_atividade: 4 }
+        ]
+    }
+  ]
+);  
