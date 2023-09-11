b957f54bc8690285e70609908e0153b110743aa6
Antonio Ferraz <101819642+ferrazton@users.noreply.github.com>
2023-09-10 21:48:08 -0300

    expansão qtd de idiomas

--- mongodb/idiomas.js
+++ mongodb/idiomas.js
@@ -1,140 +1,268 @@
 db.idiomas.insertMany([
-  {
-    _id: ObjectId("64fe33bdf758ac124aab120b"),
-    lingua: 'francês',
-    atividades: [
-      {
-        tema: 'Saudações',
-        descricao: 'Aprender a falar francês',
-        nivel: 'básico',
-        qntd_questoes: 10
-      },
-      {
-        tema: 'Cores',
-        descricao: 'Aprender as principais cores',
-        nivel: 'básico',
-        qntd_questoes: 5
-      },
-      {
-        tema: 'Alimentos',
-        descricao: 'Aprender nomes de alimentos em francês',
-        nivel: 'intermediário',
-        qntd_questoes: 8
-      },
-      {
-        tema: 'Viagem',
-        descricao: 'Vocabulário útil para viajar na França',
-        nivel: 'intermediário',
-        qntd_questoes: 7
-      }
-    ]
-  },
-  {
-    _id: ObjectId("64fe33bdf758ac124aab120c"),
-    lingua: 'inglês',
-    atividades: [
-      {
-        tema: 'Saudações',
-        descricao: 'Aprender a falar inglês',
-        nivel: 'básico',
-        qntd_questoes: 10
-      },
-      {
-        tema: 'Números',
-        descricao: 'Aprender a contar em inglês',
-        nivel: 'básico',
-        qntd_questoes: 5
-      },
-      {
-        tema: 'Família',
-        descricao: 'Vocabulário relacionado à família',
-        nivel: 'intermediário',
-        qntd_questoes: 8
-      }
-    ]
-  },
-  {
-    _id: ObjectId("64fe33bdf758ac124aab120d"),
-    lingua: 'espanhol',
-    atividades: [
-      {
-        tema: 'Saudações',
-        descricao: 'Aprender a falar espanhol',
-        nivel: 'básico',
-        qntd_questoes: 10
-      },
-      {
-        tema: 'Comida',
-        descricao: 'Aprender nomes de comidas em espanhol',
-        nivel: 'básico',
-        qntd_questoes: 6
-      },
-      {
-        tema: 'Cinema',
-        descricao: 'Vocabulário relacionado ao cinema em espanhol',
-        nivel: 'intermediário',
-        qntd_questoes: 9
-      }
-    ]
-  },
-  {
-    _id: ObjectId("64fe33bdf758ac124aab120e"),
-    lingua: 'alemão',
-    atividades: [
-      {
-        tema: 'Saudações',
-        descricao: 'Aprender a falar alemão',
-        nivel: 'básico',
-        qntd_questoes: 10
-      },
-      {
-        tema: 'Transporte',
-        descricao: 'Vocabulário relacionado a meios de transporte em alemão',
-        nivel: 'básico',
-        qntd_questoes: 7
-      },
-      {
-        tema: 'Música',
-        descricao: 'Conhecer músicas populares em alemão',
-        nivel: 'intermediário',
-        qntd_questoes: 8
-      },
-      {
-        tema: 'Cultura',
-        descricao: 'Fique por dentro da cultura alemã',
-        nivel: 'intermediário',
-        qntd_questoes: 8
-      }
-    ]
-  },
-  {
-    _id: ObjectId("64fe33bdf758ac124aab120f"),
-    lingua: 'italiano',
-    atividades: [
-      {
-        tema: 'Saudações',
-        descricao: 'Aprender a falar italiano',
-        nivel: 'básico',
-        qntd_questoes: 10
-      },
-      {
-        tema: 'Esportes',
-        descricao: 'Vocabulário relacionado a esportes em italiano',
-        nivel: 'básico',
-        qntd_questoes: 6
-      },
-      {
-        tema: 'Arte',
-        descricao: 'Conhecer obras de arte italianas famosas',
-        nivel: 'intermediário',
-        qntd_questoes: 9
-      },
-      {
-        tema: 'Culinaria',
-        descricao: 'Aprender nomes de comidas em italiano',
-        nivel: 'intermediário',
-        qntd_questoes: 9
-      }
-    ]
-  }
-]);
+    {
+      _id: ObjectId("64fe33bdf758ac124aab120b"),
+      lingua: 'francês',
+      atividades: [
+        {
+          tema: 'Saudações',
+          descricao: 'Aprender a falar francês',
+          nivel: 'básico',
+          qntd_questoes: 10
+        },
+        {
+          tema: 'Cores',
+          descricao: 'Aprender as principais cores',
+          nivel: 'básico',
+          qntd_questoes: 5
+        },
+        {
+          tema: 'Alimentos',
+          descricao: 'Aprender nomes de alimentos em francês',
+          nivel: 'intermediário',
+          qntd_questoes: 8
+        },
+        {
+          tema: 'Viagem',
+          descricao: 'Vocabulário útil para viajar na França',
+          nivel: 'intermediário',
+          qntd_questoes: 7
+        }
+      ]
+    },
+    {
+      _id: ObjectId("64fe33bdf758ac124aab120c"),
+      lingua: 'inglês',
+      atividades: [
+        {
+          tema: 'Saudações',
+          descricao: 'Aprender a falar inglês',
+          nivel: 'básico',
+          qntd_questoes: 10
+        },
+        {
+          tema: 'Números',
+          descricao: 'Aprender a contar em inglês',
+          nivel: 'básico',
+          qntd_questoes: 5
+        },
+        {
+          tema: 'Família',
+          descricao: 'Vocabulário relacionado à família',
+          nivel: 'intermediário',
+          qntd_questoes: 8
+        }
+      ]
+    },
+    {
+      _id: ObjectId("64fe33bdf758ac124aab120d"),
+      lingua: 'espanhol',
+      atividades: [
+        {
+          tema: 'Saudações',
+          descricao: 'Aprender a falar espanhol',
+          nivel: 'básico',
+          qntd_questoes: 10
+        },
+        {
+          tema: 'Comida',
+          descricao: 'Aprender nomes de comidas em espanhol',
+          nivel: 'básico',
+          qntd_questoes: 6
+        },
+        {
+          tema: 'Cinema',
+          descricao: 'Vocabulário relacionado ao cinema em espanhol',
+          nivel: 'intermediário',
+          qntd_questoes: 9
+        }
+      ]
+    },
+    {
+      _id: ObjectId("64fe33bdf758ac124aab120e"),
+      lingua: 'alemão',
+      atividades: [
+        {
+          tema: 'Saudações',
+          descricao: 'Aprender a falar alemão',
+          nivel: 'básico',
+          qntd_questoes: 10
+        },
+        {
+          tema: 'Transporte',
+          descricao: 'Vocabulário relacionado a meios de transporte em alemão',
+          nivel: 'básico',
+          qntd_questoes: 7
+        },
+        {
+          tema: 'Música',
+          descricao: 'Conhecer músicas populares em alemão',
+          nivel: 'intermediário',
+          qntd_questoes: 8
+        },
+        {
+          tema: 'Cultura',
+          descricao: 'Fique por dentro da cultura alemã',
+          nivel: 'intermediário',
+          qntd_questoes: 8
+        }
+      ]
+    },
+    {
+      _id: ObjectId("64fe33bdf758ac124aab120f"),
+      lingua: 'italiano',
+      atividades: [
+        {
+          tema: 'Saudações',
+          descricao: 'Aprender a falar italiano',
+          nivel: 'básico',
+          qntd_questoes: 10
+        },
+        {
+          tema: 'Esportes',
+          descricao: 'Vocabulário relacionado a esportes em italiano',
+          nivel: 'básico',
+          qntd_questoes: 6
+        },
+        {
+          tema: 'Arte',
+          descricao: 'Conhecer obras de arte italianas famosas',
+          nivel: 'intermediário',
+          qntd_questoes: 9
+        },
+        {
+          tema: 'Culinaria',
+          descricao: 'Aprender nomes de comidas em italiano',
+          nivel: 'intermediário',
+          qntd_questoes: 9
+        }
+      ]
+    },
+    {
+        lingua: 'japonês',
+        atividades: [
+            {
+                tema: 'Saudações',
+                descricao: 'Aprender a falar japonês',
+                nivel: 'básico',
+                qntd_questoes: 10
+            },
+            {
+                tema: 'Comida',
+                descricao: 'Aprender nomes de comidas em japonês',
+                nivel: 'básico',
+                qntd_questoes: 6
+            },
+            {
+                tema: 'Cinema',
+                descricao: 'Vocabulário relacionado ao cinema em japonês',
+                nivel: 'intermediário',
+                qntd_questoes: 9
+            },
+            {
+                tema: 'Cultura',
+                descricao: 'Fique por dentro da cultura japonesa',
+                nivel: 'intermediário',
+                qntd_questoes: 8
+            }
+        ]
+    },
+    {
+        lingua: 'coreano',
+        atividades: [
+            {
+                tema: 'Saudações',
+                descricao: 'Aprender a falar coreano',
+                nivel: 'básico',
+                qntd_questoes: 10
+            },
+            {
+                tema: 'Comida',
+                descricao: 'Aprender nomes de comidas em coreano',
+                nivel: 'básico',
+                qntd_questoes: 6
+            },
+            {
+                tema: 'Cinema',
+                descricao: 'Vocabulário relacionado ao cinema em coreano',
+                nivel: 'intermediário',
+                qntd_questoes: 9
+            },
+            {
+                tema: 'Arte',
+                descricao: 'Conhecer obras de arte coreanas famosas',
+                nivel: 'intermediário',
+                qntd_questoes: 9
+            },
+            {
+                tema: 'Música',
+                descricao: 'Conhecer músicas populares em coreano',
+                nivel: 'intermediário',
+                qntd_questoes: 8
+            }
+        ]
+    },
+    {
+        lingua: 'árabe',
+        atividades: [
+            {
+                tema: 'Saudações',
+                descricao: 'Aprender a falar árabe',
+                nivel: 'básico',
+                qntd_questoes: 10
+            },
+            {
+                tema: 'Números',
+                descricao: 'Aprender a contar em árabe',
+                nivel: 'básico',
+                qntd_questoes: 5
+            },
+            {
+                tema: 'Família',
+                descricao: 'Vocabulário relacionado à família',
+                nivel: 'intermediário',
+                qntd_questoes: 8
+            },
+            {
+                tema: 'Cultura',
+                descricao: 'Fique por dentro da cultura árabe',
+                nivel: 'intermediário',
+                qntd_questoes: 8
+            }
+        ]
+    },
+    {
+        lingua: 'latim',
+        atividades: [
+            {
+                tema: 'Saudações',
+                descricao: 'Aprender a falar latim',
+                nivel: 'básico',
+                qntd_questoes: 10
+            },
+            {
+                tema: 'Números',
+                descricao: 'Aprender a contar em latim',
+                nivel: 'básico',
+                qntd_questoes: 5
+            },
+            {
+                tema: 'Comida',
+                descricao: 'Aprender nomes de comidas em latim',
+                nivel: 'básico',
+                qntd_questoes: 6
+            },
+            {
+                tema: 'Animais',
+                descricao: 'Vocabulário relacionado a animais em latim',
+                nivel: 'intermediário',
+                qntd_questoes: 9
+            },
+            {
+                tema: 'Família',
+                descricao: 'Vocabulário relacionado à família',
+                nivel: 'intermediário',
+                qntd_questoes: 8
+            }
+        ]
+    }
+  ]);
